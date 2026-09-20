32e03b19483630ca53c155a9753991132a8fef48
IT26101915 <it26101915@my.sliit.lk>
2026-09-20 05:24:12 -0700

    Lab8Q2

---
+++ IT26101915Lab8Q2.java
@@ -0,0 +1,20 @@
+public class IT26101915Lab8Q2 {
+    public static void main(String[] args) {
+
+        int[] A = {10, 20, 30, 40, 50};
+        int[] B = {34, 67, 12, 89, 12};
+        int[] C = new int[5];
+
+        // Add A and B
+        for (int i = 0; i < 5; i++) {
+            C[i] = A[i] + B[i];
+        }
+
+        // Display C
+        System.out.println("Array C:");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(C[i]);
+        }
+    }
+}
